50d4a4503d502fb85d67476fcf37abec2e6d2a15
quminzhennna <192948673@qq.com>
2026-01-20 15:21:50 +0800

    docs(PVZString):comment

--- pvzclass/PVZ.h
+++ pvzclass/PVZ.h
@@ -142,6 +142,9 @@ namespace PVZ
 		/// @return 创建的字符串
 		/// @author Moon404
 		static PVZString Make(const char* str);
+		/// @brief 将给定的字符串转换为对应的 PVZ 字符串
+		/// @param str 字符串
+		/// @return 转换后的字符串
 		static PVZString ToString(const char* str);
 		/// @brief 将给定的“[X]”格式字符数组转换为对应的译文字符串
 		/// @note 不符合格式时，会直接转换为字符串。
