cf5be9fbd1ab822a42a640f56c0f617fd2d535f8
quminzhennan <192948673@qq.com>
2026-01-30 11:54:23 +0800

    feat(Event):ZombieEatDamagePlantEvent

--- pvzclass/include/Events/ZombieEatEvent.h
+++ pvzclass/include/Events/ZombieEatEvent.h
@@ -43,4 +43,29 @@ namespace PVZEvent
 			p2->end();
 		}
 	};
+
+	/// @brief 僵尸啃食植物时，植物受到伤害事件
+	/// @param zombie，plant, damage
+	/// @note 此函数会重载原版受伤的代码
+	/// @return 重载后的伤害，负数会取消伤害
+	/// @todo 将修改伤害的功能使用传入引用实现，植物是否受到伤害改用返回值返回ThreeState判断
+	class ZombieEatDamagePlantEvent : public DLLEventTemplate<0x52FCF0, 7, CONST_VAL(4), REG_ESI, REG_EBP>
+	{
+	public:
+		ZombieEatDamagePlantEvent() : DLLEventTemplate() { Init("onZombieEatDamagePlant"); };
+		ZombieEatDamagePlantEvent(const char* str) : DLLEventTemplate() { Init(str); };
+		ZombieEatDamagePlantEvent(int address) : DLLEventTemplate() { Init(address); };
+	protected:
+		inline static constexpr byte asm_code[3] = { 0x29, 0x46, 0x40 };//sub [esi+40h] eax
+		virtual void InitExtra(AsmBuilder& builder) override
+		{
+			builder.cmp_reg_imm(REG_EAX, 0)
+				.js_rel(3)
+				.add_bytes(asm_code, 3)
+				.popad()
+				.mov_reg_mem_reg_add_imm(REG_ECX, REG_ESI, 0x40)
+				.push_imm32(0x52FCF7)
+				.ret();
+		}
+	};
 }
